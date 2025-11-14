474190b5f13fc0f7973333e7da979848ffdf18de
Abdelrahman.Youssef <abderlrhmanyoussef@gmail.com>
2025-11-14 13:12:02 +0200

    initial Xcode project

---
+++ CountryExplorer/CountryExplorer/CountryExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryExplorerApp.swift
+//  CountryExplorer
+//
+//  Created by Abdelrahman.Youssef on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct CountryExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
